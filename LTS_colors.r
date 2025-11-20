56d599e34e14100038392156aafe0118f470701e
RademacherTim <tim.rademacher@uvm.edu>
2025-11-20 11:35:55 -0500

    Add uniform colors to repository

---
+++ LTS_colors.r
@@ -0,0 +1,4 @@
+# Uniform colors for the data ----
+LTS_colors <- tibble(t = 1:4,
+                     t_names = c("control", "gravity", "vacuum", "sugar"),
+                     color = c("#98FF98", "#1E90FF", "#004080", "#9B59B6"))
